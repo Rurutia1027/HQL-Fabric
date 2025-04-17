81c942b355408214462bdbb86343b6b65fe7c651
Rurutia1027 <rurutia1027@gmail.com>
2025-04-17 16:30:49 +0800

    add select and where caluse enumeration defintions

---
+++ hql-fabric/hql-fabric-core/src/main/java/com/hql/fabric/persistence/query/builder/SelectClause.java
@@ -0,0 +1,30 @@
+package com.hql.fabric.persistence.query.builder;
+
+public enum SelectClause {
+    SELECT,
+    SELECT_MAP(true),
+    UPDATE,
+    INSERT,
+    DELETE,
+    AVERAGE(true),
+    COUNT(true),
+    MAX(true),
+    MIN(true);
+
+    private boolean fn = false;
+
+    SelectClause(boolean fn) {
+        this.fn = fn;
+    }
+
+    SelectClause() {
+    }
+
+    /**
+     * @return Whether the selection clause is a function that
+     * takes an operator field. E.g., "count(entity)" versus "select entity".
+     */
+    public boolean isFn() {
+        return fn;
+    }
+}
---
+++ hql-fabric/hql-fabric-core/src/main/java/com/hql/fabric/persistence/query/builder/WhereClause.java
@@ -0,0 +1,25 @@
+package com.hql.fabric.persistence.query.builder;
+
+public enum WhereClause {
+    EQUALS,
+    IN,
+    /**
+     * This gonna be used in other types of 'in', like elements/index querying.
+     */
+    IN_NO_PARENS,
+    LIKE,
+    AND,
+    OR,
+    OPEN_SCOPE,
+    CLOSE_SCOPE,
+    NULL,
+    NOT_NULL,
+    MAP,
+    SUB_QUERY,
+    GREATER_THAN,
+    LESS_THAN,
+    NOT_EQUAL,
+    GREATER_EQUAL_THAN,
+    NOT,
+    LESS_EQUAL_THAN
+}
